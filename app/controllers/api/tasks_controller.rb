45f877fb1237a21cbc6d1633cb31692c1fd9afea
aslanshaken <aslanshaken@gmail.com>
2024-02-24 12:14:51 -0600

    add and update task API

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,13 +1,40 @@
 # app/controllers/api/tasks_controller.rb
 class Api::TasksController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create, :update]
   before_action :set_task, only: [:show, :update, :destroy, :assign, :progress]
 
-  # POST /api/tasks
   def create
+    # Post /api/tasks
+    # {
+    #   "task": {
+    #     "title": "Complete Project Proposal",
+    #     "description": "Draft the project proposal for client review.",
+    #     "due_date": "2024-03-15"
+    #   }
+    # }
+
+    @task = Task.new(task_params)
+
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
   end
 
-  # PUT /api/tasks/{taskId}
   def update
+    # Put /api/tasks/{taskId}
+    # {
+    #   "task": {
+    #     "description": "Finalize the project proposal for client review."
+    #   }
+    # }
+
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
   end
 
   # DELETE /api/tasks/{taskId}
@@ -24,5 +51,15 @@ class Api::TasksController < ApplicationController
 
   # GET /api/tasks
   def index
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :due_date, :status, :completed_date)
   end
 end
