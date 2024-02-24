475d54a9e5a518d265c528c6bc87e40d90e963e6
aslanshaken <aslanshaken@gmail.com>
2024-02-24 11:55:43 -0600

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+require 'faker'
+
+Assignment.destroy_all
+Task.destroy_all
+User.destroy_all
+
+5.times do
+  User.create( name: Faker::Name.name, email: Faker::Internet.email)
+end
+
+10.times do
+  Task.create(title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+    due_date: Faker::Time.forward(days: 30),
+    status: ['Pending', 'In Progress', 'Completed'].sample,
+    priority: ['High', 'Medium', 'Low'].sample
+  )
+end
+
+Task.all.each do |task|
+  user = User.offset(rand(User.count)).first
+  Assignment.create(task_id: task.id, user_id: user.id)
+end
